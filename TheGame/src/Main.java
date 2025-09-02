373733ee5aec4c668cabdadf02fa96fdd20c3a3a
FabioLCCardoso <fabioluiz.casanova@gmail.com>
2025-09-02 18:51:11 -0300

    Starting project

--- TheGame/src/Main.java
+++ TheGame/src/Main.java
@@ -1,13 +1,13 @@
 import javax.swing.JFrame; // Import the JFrame class
 
-public class game {
+public class Main {
 
     public static void main(String[] args) {
         // Create a new JFrame object
-        JFrame frame = new JFrame("My First Window"); // Set the window title
+        JFrame frame = new JFrame("Cumshots <3"); // Set the window title
 
         // Set the size of the window (width, height in pixels)
-        frame.setSize(400, 300);
+        frame.setSize(800, 600);
 
         // Specify what happens when the user closes the window
         // EXIT_ON_CLOSE terminates the application when the window is closed
